074fd9dc491d1a12166c73a5a173c28fa62737eb
tainzhi <qfq61@qq.com>
2023-06-06 07:19:34 +0800

    添加能在win10中terminal启动配置

--- start-kanata.bat
+++ start-kanata.bat
@@ -1,2 +1,12 @@
+@echo off
+@REM https://bbs.pcbeta.com/viewthread-1959786-1-1.html
+@REM 下面的方法会导致启动两个terminal
+@REM cmd启动速度是快于terminal的
+@REM 从windows terminal打开改bat, reference: https://blog.csdn.net/weixin_45610260/article/details/117933344
+@REM if not defined bflag set bflag=1 && start wt --title "Keyboard Remap" %0 && exit 
+cd /d %~dp0
 
-D:\KeyboardRemaps\kanata_wintercept_cmd_allowed.exe --cfg D:\KeyboardRemaps\kanata.kbd
+set CURRENT_DIR=%cd%
+%CURRENT_DIR%\kanata_wintercept_cmd_allowed.exe --cfg %CURRENT_DIR%\kanata.kbd
+
+@REM C:\Users\muqing\Documents\KeyboardRemaps\kanata_wintercept_cmd_allowed.exe --cfg C:\Users\muqing\Documents\KeyboardRemaps\kanata.kbd
